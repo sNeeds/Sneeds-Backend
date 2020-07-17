20be9f81ddb3a5f4a05fc2725ab1b6c5ab4c924d
aryabartar <bartararya@gmail.com>
2020-07-17 13:02:38 +0430

    Update settings

--- code/sNeeds/utils/custom/custom_functions.py
+++ code/sNeeds/utils/custom/custom_functions.py
@@ -3,9 +3,6 @@ import datetime
 from django.contrib.auth import get_user_model
 
 
-
-
-
 def student_info_year_choicess():
     return [(r, r) for r in range(2000, datetime.date.today().year + 4)]
 
