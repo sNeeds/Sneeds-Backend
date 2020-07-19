7e51565321628e1ef1540cc49a5d16a4f1e59520
aryabartar <bartararya@gmail.com>
2020-07-19 06:54:44 -0600

    Update University

--- code/sNeeds/apps/account/models.py
+++ code/sNeeds/apps/account/models.py
@@ -47,7 +47,7 @@ class University(models.Model):
     name = models.CharField(max_length=256, unique=True)
     country = models.ForeignKey(Country, null=True, blank=False, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
-    picture = models.ImageField(upload_to=get_image_upload_path("university-pictures"))
+    picture = models.ImageField(null=True, upload_to=get_image_upload_path("university-pictures"))
     rank = models.PositiveIntegerField(blank=True, null=True)
     is_college = models.BooleanField(default=False)
 
