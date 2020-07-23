bf87b3485f52eca7717f96c976b3926dffa949bb
aryabartar <bartararya@gmail.com>
2020-07-23 14:39:53 +0430

    Fix SoldTimeSlotChangedEmailNotification admin bug

--- code/sNeeds/apps/notifications/admin.py
+++ code/sNeeds/apps/notifications/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Notification, EmailNotification, SoldTimeSlotReminderEmailNotification
+from .models import Notification, EmailNotification, SoldTimeSlotReminderEmailNotification, \
+    SoldTimeSlotChangedEmailNotification
 
 
 @admin.register(EmailNotification)
