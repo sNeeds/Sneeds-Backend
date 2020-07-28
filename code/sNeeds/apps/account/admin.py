d03b35befb21911d2c71fb10215ab325a381f9a9
aryabartar <bartararya@gmail.com>
2020-07-28 11:42:31 +0430

    Update account adming

--- code/sNeeds/apps/account/admin.py
+++ code/sNeeds/apps/account/admin.py
@@ -6,6 +6,7 @@ admin.site.register(models.Country)
 admin.site.register(models.UniversityThrough)
 admin.site.register(models.FieldOfStudyType)
 admin.site.register(models.Publication)
+admin.site.register(models.WantToApply)
 # admin.site.register(models.GRECertificate)
 # admin.site.register(models.GMATCertificate)
 
