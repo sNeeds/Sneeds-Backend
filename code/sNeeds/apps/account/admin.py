bd201925f9a6583d8962b7d4d8293a88000ea606
aryabartar <bartararya@gmail.com>
2020-08-03 22:38:07 +0430

    Fix admin.site.register(models.StudentDetailedInfo)  bug

--- code/sNeeds/apps/account/admin.py
+++ code/sNeeds/apps/account/admin.py
@@ -5,7 +5,6 @@ from . import models
 admin.site.register(models.University)
 admin.site.register(models.FieldOfStudy)
 admin.site.register(models.Country)
-admin.site.register(models.StudentDetailedInfo)
 admin.site.register(models.StudentFormApplySemesterYear)
 
 
