4a4c17b8e9acc14d68e9d189889188fb6d67e042
aryabartar <bartararya@gmail.com>
2020-09-01 14:30:40 +0430

    Add AppliedTo

--- code/sNeeds/apps/similarApply/managers.py
+++ code/sNeeds/apps/similarApply/managers.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class AppliedStudentDetailedInfoManager(models.QuerySet):
-   def get_applied_to_universities(self, universities_qs):
-      universities = [u.id for u in universities_qs]
-      # return self.filter(want_to_apply)
+    def get_applied_to_universities(self, universities_qs):
+        universities = [u.id for u in universities_qs]
+        return self.filter(universities__in=universities)
+
+    def get_applied_to_universities(self, universities_qs):
+        universities = [u.id for u in universities_qs]
+        return self.filter(universities__in=universities)
--- code/sNeeds/apps/similarApply/models.py
+++ code/sNeeds/apps/similarApply/models.py
@@ -1,8 +1,41 @@
 from django.db import models
 
-from sNeeds.apps.account.models import StudentDetailedInfoBase
+from sNeeds.apps.account.models import StudentDetailedInfoBase, Country, University, GradeModel, FieldOfStudy, \
+    StudentFormApplySemesterYear
+
+
+class AppliedTo(models.Model):
+    student_detailed_info = models.OneToOneField(
+        'AppliedStudentDetailedInfo',
+        on_delete=models.CASCADE,
+        related_name="applied_to"
+    )
+
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE
+    )
+
+    university = models.ForeignKey(
+        University,
+        on_delete=models.CASCADE
+    )
+
+    grade = models.ForeignKey(
+        GradeModel,
+        on_delete=models.CASCADE,
+    )
+
+    major = models.ForeignKey(
+        FieldOfStudy,
+        on_delete=models.CASCADE
+    )
+
+    semester_year = models.ForeignKey(
+        StudentFormApplySemesterYear,
+        on_delete=models.PROTECT
+    )
 
 
 class AppliedStudentDetailedInfo(StudentDetailedInfoBase):
     pass
-
