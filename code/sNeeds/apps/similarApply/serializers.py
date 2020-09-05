5485e928c46dd6b5ea9a560951530137c61a29ab
aryabartar <bartararya@gmail.com>
2020-09-05 13:02:05 +0430

    Add AppliedToSerializer

--- code/sNeeds/apps/similarApply/serializers.py
+++ code/sNeeds/apps/similarApply/serializers.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
 
-from sNeeds.apps.similarApply.models import AppliedStudentDetailedInfo
+from sNeeds.apps.account.serializers import StudentDetailedInfoBaseSerializer
+from sNeeds.apps.similarApply.models import AppliedStudentDetailedInfo, AppliedTo
 
 
-class AppliedStudentDetailedInfoSerializer(serializers.ModelSerializer):
+class AppliedToSerializer(serializers.ModelSerializer):
     class Meta:
+        fields = [
+            'id', 'student_detailed_info',
+            'country', 'university', 'grade', 'major', 'semester_year', 'fund',
+            'accepted', 'comment'
+        ]
+
+
+class AppliedStudentDetailedInfoSerializer(StudentDetailedInfoBaseSerializer):
+    applied_to = serializers.SerializerMethodField()
+
+    class Meta(StudentDetailedInfoBaseSerializer.Meta):
         model = AppliedStudentDetailedInfo
-        fields = ['id', 'universities', 'discount', 'url', 'code', ]
+        fields = StudentDetailedInfoBaseSerializer.Meta.fields + [
+            'applied_to',
+        ]
+
+    def get_applied_to(self, obj):
+        try:
+            applied_to_obj = AppliedTo.objects.get(student_detailed_info__id=obj.id)
+            data = AppliedToSerializer(applied_to_obj)
+        except AppliedTo.DoesNotExist:
+            data = None
+
+        return data
