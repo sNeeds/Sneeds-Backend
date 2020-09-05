80e9aaf36ec8e5174ae487f51161a91a4a6cc086
aryabartar <bartararya@gmail.com>
2020-09-05 16:10:20 +0430

    Update SimilarUniversitiesListView

--- code/sNeeds/apps/similarApply/views.py
+++ code/sNeeds/apps/similarApply/views.py
@@ -3,14 +3,15 @@ import uuid
 from django.http import Http404
 from rest_framework import generics
 from rest_framework.exceptions import APIException
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from sNeeds.apps.account.models import StudentDetailedInfo
+from sNeeds.apps.account.models import StudentDetailedInfo, WantToApply
 from sNeeds.apps.similarApply.models import AppliedStudentDetailedInfo
+from sNeeds.apps.similarApply.serializers import AppliedStudentDetailedInfoSerializer
 
 
-class SimilarUniversitiesListView(generics.RetrieveAPIView):
-    serializer_class = None
+class SimilarUniversitiesListView(APIView):
 
     def get_form_obj(self, form_id):
         try:
@@ -21,15 +22,18 @@ class SimilarUniversitiesListView(generics.RetrieveAPIView):
     def get(self, request, form_id, format=None):
 
         form = self.get_form_obj(form_id)
-        origin_universities = form.universities.all()
-
-        # Want to apply is 1 to 1 with form
-        want_to_apply = form.want_to_apply.all().first()
-        want_to_apply_universities = want_to_apply.universities.all()
-
         related_forms = AppliedStudentDetailedInfo.objects.none()
 
+        origin_universities = form.universities.all()
         related_forms |= AppliedStudentDetailedInfo.objects.same_origin_universities(origin_universities)
-        related_forms |= AppliedStudentDetailedInfo.objects.applied_to_universities(want_to_apply_universities)
 
-        return None
+        try:
+            # Want to apply is 1 to 1 with form
+            want_to_apply = WantToApply.objects.get(student_detailed_info__id=form.id)
+            want_to_apply_universities = want_to_apply.universities.all()
+            related_forms |= AppliedStudentDetailedInfo.objects.applied_to_universities(want_to_apply_universities)
+        except WantToApply.DoesNotExist:
+            pass
+
+        data = AppliedStudentDetailedInfoSerializer(related_forms, many=True).data
+        return Response(data)
