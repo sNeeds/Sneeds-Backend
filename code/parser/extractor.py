ee7fe0d0f7409fb91652cd1bbe7f49b56e7fd723
aryabartar <bartararya@gmail.com>
2020-09-16 19:14:02 +0430

    Add h4 extractor

--- code/parser/extractor.py
+++ code/parser/extractor.py
@@ -7,9 +7,9 @@ class Node:
         self.content = content
 
     def __str__(self):
-        name = "Str: " + self.content
+        name = self.content
         if self.parent:
-            name += " | Parent: " + str(self.parent)
+            name +=  " -> " + self.parent.__str__()
         return name
 
 content = None
@@ -18,6 +18,7 @@ nodes_list = []
 
 current_h2_node = None
 current_h3_node = None
+current_h4_node = None
 
 with open('majors.html', 'r') as f:
     content = f.read()
@@ -40,6 +41,15 @@ def check_and_add_h3_header(e):
         current_h3_node = node
 
 
+def check_and_add_h4_header(e):
+    if e.name == 'h4':
+        global current_h4_node
+        text = e.select("span.mw-headline")[0].get_text()
+        node = Node(text, parent=current_h3_node)
+        nodes_list.append(node)
+        current_h4_node = node
+
+
 soup = BeautifulSoup(content, 'lxml')
 
 main_body = soup.select('body div#content div#bodyContent div#mw-content-text div.mw-parser-output')[0]
@@ -48,6 +58,7 @@ for e in main_body.children:
     if e.name:
         check_and_add_h2_header(e)
         check_and_add_h3_header(e)
+        check_and_add_h4_header(e)
 
 for n in nodes_list:
     print(n)
