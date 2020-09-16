d40453377d0f4ecfe0c8ace94bb674ca3d572919
aryabartar <bartararya@gmail.com>
2020-09-16 19:10:09 +0430

    Add h2 and h3 extractor

--- code/parser/extractor.py
+++ code/parser/extractor.py
@@ -2,17 +2,52 @@ from bs4 import BeautifulSoup
 
 
 class Node:
-    def __init__(self, parent=None, mode=None, content=None):
+    def __init__(self, content, parent=None):
         self.parent = parent
-        self.mode = mode
         self.content = content
 
+    def __str__(self):
+        name = "Str: " + self.content
+        if self.parent:
+            name += " | Parent: " + str(self.parent)
+        return name
 
 content = None
+
+nodes_list = []
+
+current_h2_node = None
+current_h3_node = None
 
 with open('majors.html', 'r') as f:
     content = f.read()
 
+
+def check_and_add_h2_header(e):
+    if e.name == 'h2':
+        global current_h2_node
+        node = Node(e.span.get_text())
+        nodes_list.append(node)
+        current_h2_node = node
+
+
+def check_and_add_h3_header(e):
+    if e.name == 'h3':
+        global current_h3_node
+        text = e.select("span.mw-headline")[0].get_text()
+        node = Node(text, parent=current_h2_node)
+        nodes_list.append(node)
+        current_h3_node = node
+
+
 soup = BeautifulSoup(content, 'lxml')
 
-print(soup.find_all('a'))
+main_body = soup.select('body div#content div#bodyContent div#mw-content-text div.mw-parser-output')[0]
+
+for e in main_body.children:
+    if e.name:
+        check_and_add_h2_header(e)
+        check_and_add_h3_header(e)
+
+for n in nodes_list:
+    print(n)
