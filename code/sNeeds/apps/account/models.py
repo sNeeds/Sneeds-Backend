95e74b0637c28c45a485f1baf7020d4e070f025c
hossein_7 <hossein.slayer@gmail.com>
2020-09-25 15:53:59 +0330

    bug fixes

--- code/sNeeds/apps/account/models.py
+++ code/sNeeds/apps/account/models.py
@@ -1036,13 +1036,15 @@ class GMATCertificate(LanguageCertificate):
 
     def get_view_label(self):
         item_range = self.TOTAL_VIEW_LABEL_RANGE
-        return str(floor(self.total / item_range) * item_range)
+        value = floor(self.total / item_range) * item_range
+        return str(value) + '-' + str(value + item_range)
 
     @classmethod
     def convert_store_to_view_label(cls, label):
-        value = int(label)
+        input_value = int(label)
         item_range = cls.TOTAL_VIEW_LABEL_RANGE
-        return str(floor(value / item_range) * item_range)
+        value = floor(input_value / item_range) * item_range
+        return str(value) + '-' + str(value + item_range)
 
     @classmethod
     def get_user_store_based_positions(cls, sdi):
