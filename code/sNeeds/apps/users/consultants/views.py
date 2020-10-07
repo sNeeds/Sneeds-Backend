3fed9aaa557bd408689338115b975f12d781335a
hossein_7 <hossein.slayer@gmail.com>
2020-10-07 15:46:02 +0330

    some edits for search

--- code/sNeeds/apps/users/consultants/views.py
+++ code/sNeeds/apps/users/consultants/views.py
@@ -61,12 +61,12 @@ class ConsultantProfileList(generics.ListAPIView):
 
         qs = filter_consultants(qs, university, country, major)
 
+        qs = search_consultants(qs, search_phrase)
+
         if self.request.query_params.get("ordering") is not None:
             if "-rate" in self.request.query_params.get("ordering"):
                 qs = qs.order_by(F('rate').desc(nulls_last=True))
             elif "rate" in self.request.query_params.get("ordering"):
                 qs = qs.order_by(F('rate').asc(nulls_first=True))
-
-        qs = search_consultants(qs, search_phrase)
 
         return qs
