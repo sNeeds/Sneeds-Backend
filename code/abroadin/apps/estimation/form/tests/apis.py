02b1c56c987f505e371db731a57c0c5378caae5c
aryabartar <bartararya@gmail.com>
2020-10-21 18:00:32 +0330

    Update StudentDetailedInfoTests

--- code/abroadin/apps/estimation/form/tests/apis.py
+++ code/abroadin/apps/estimation/form/tests/apis.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from rest_framework import status
 
 from abroadin.apps.estimation.tests.apis import EstimationBaseTest
+from apps.estimation.form.models import StudentDetailedInfo
 
 User = get_user_model()
 
@@ -50,7 +51,7 @@ class StudentDetailedInfoTests(EstimationBaseTest):
         self._test_form_list("post", None, status.HTTP_201_CREATED)
         self._test_form_list("post", None, status.HTTP_201_CREATED)
         self._test_form_list("post", self.user1, status.HTTP_201_CREATED)
-        self._test_form_list("post", self.user2, status.HTTP_201_CREATED)
+        self._test_form_list("post", self.user2, status.HTTP_201_CREATED, data={"age": 20}, )
 
     def test_form_list_post_403(self):
         self._test_form_list("post", self.user1, status.HTTP_201_CREATED)
@@ -58,32 +59,36 @@ class StudentDetailedInfoTests(EstimationBaseTest):
         self._test_form_list("post", self.user1, status.HTTP_403_FORBIDDEN)
 
     def test_form_detail_put_200(self):
+        update_these = {
+            "age": 20,
+            "related_work_experience": 5,
+            "academic_break": 5,
+            "olympiad": "Foo olympiad",
+            "gender": "Male",
+            "payment_affordability": "High",
+            "prefers_full_fund": True,
+            "prefers_half_fund": False,
+            "prefers_self_fund": True,
+            "comment": "Foo comment",
+            "powerful_recommendation": True,
+            "linkedin_url": "https://www.linkedin.com/in/arya-khaligh/",
+            "homepage_url": "https://www.aryakhaligh.ir/",
+        }
+
         data = self._test_form_list("post", self.user1, status.HTTP_201_CREATED)
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"age": 20}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"related_work_experience": 5}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"academic_break": 5}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"olympiad": 5}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"gender": "Male"}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"is_married": True}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"payment_affordability": "Average"}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"prefers_full_fund": True}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"prefers_half_fund": False}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"prefers_self_fund": True}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"comment": "Foo comment"}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"powerful_recommendation": False}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
-                               data={"linkedin_url": "https://www.linkedin.com/in/arya-khaligh/"}, )
-        self._test_form_detail("put", self.user1, status.HTTP_200_FORBIDDEN, reverse_args=data['id'],
-                               data={"homepage_url": "https://www.aryakhaligh.ir/"}, )
+        for k, v in update_these.items():
+            self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'], data={k: v}, )
+        #
+        # obj = StudentDetailedInfo.objects.filter(id=data['id'])
+        # self.assertEqual(obj.age, data['age'])
+        # self.assertEqual(obj.related_work_experience, data['related_work_experience'])
+        # self.assertEqual(obj.academic_break, data['academic_break'])
+        # self.assertEqual(obj.olympiad, data['olympiad'])
+        # self.assertEqual(obj.gender, data['gender'])
+        # self.assertEqual(obj.is_married, data['age'])
+        # self.assertEqual(obj.age, data['age'])
+        # self.assertEqual(obj.age, data['age'])
+        #
+        # data = self._test_form_list("post", None, status.HTTP_201_CREATED)
+        # self._test_form_detail("put", self.user1, status.HTTP_200_OK, reverse_args=data['id'],
+        #                        data={"homepage_url": "https://www.aryakhaligh.ir/"}, )
