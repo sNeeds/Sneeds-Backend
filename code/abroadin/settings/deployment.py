ae3a6a2bdc62102ef7ef6545fbc20c408035f302
aryabartar <bartararya@gmail.com>
2020-10-24 21:36:02 +0330

    Update ALLOWED_HOSTS

--- code/abroadin/settings/deployment.py
+++ code/abroadin/settings/deployment.py
@@ -2,5 +2,5 @@ from .settings import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '130.185.123.199',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '130.185.123.199', 'api.abroadin.com']
 
