8cea6f2f711359259c4b73dd5a7e9150e9c95e14
hossein_7 <hossein.slayer@gmail.com>
2020-10-25 23:12:26 +0330

    language charts .as_view added :|||

--- code/abroadin/apps/estimation/analyze/urls.py
+++ code/abroadin/apps/estimation/analyze/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('<uuid:student_form>/publication/', views.PublicationChartsRetrieveAPIView.as_view(),
          name='publication-charts'),
 
-    path('<uuid:student_form>/language-certificates/', views.LanguageCertificatesRetrieveAPIView,
+    path('<uuid:student_form>/language-certificates/', views.LanguageCertificatesRetrieveAPIView.as_view(),
          name='language-certificates-charts'),
 
     path('<uuid:student_form>/other/', views.OtherChartsRetrieveAPIView.as_view(),
