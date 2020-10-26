97d91403593a589d88851204e6e8a7ac1edd0575
aryabartar <bartararya@gmail.com>
2020-10-26 17:11:39 +0330

    Update University

--- code/abroadin/apps/data/account/models.py
+++ code/abroadin/apps/data/account/models.py
@@ -38,7 +38,7 @@ class Country(models.Model):
 class University(models.Model):
     name = models.CharField(max_length=256, unique=True)
     search_name = models.CharField(blank=False, max_length=1024, unique=True)
-    country = models.ForeignKey(Country, null=True, blank=True, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
     picture = models.ImageField(null=True, blank=True, upload_to=get_image_upload_path("university-pictures"))
     rank = models.PositiveIntegerField()
