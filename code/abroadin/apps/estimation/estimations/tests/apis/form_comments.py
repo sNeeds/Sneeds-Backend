d5cac85dddddcf3dddcab8a20648a39f29ad07b3
aryabartar <bartararya@gmail.com>
2020-10-26 09:31:10 +0330

    Add FormCommentsAPITests

--- code/abroadin/apps/estimation/estimations/tests/apis/form_comments.py
+++ code/abroadin/apps/estimation/estimations/tests/apis/form_comments.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 
 from abroadin.apps.data.account.models import Country, University, Major
+from apps.estimation.estimations.tests.apis.base import EstimationsAppAPITests
 from apps.estimation.estimations.tests.base import EstimationsAppBaseTests
 from abroadin.apps.estimation.form.models import (
     StudentDetailedInfo,
@@ -21,7 +22,12 @@ from abroadin.apps.estimation.form.models import (
 User = get_user_model()
 
 
-class EstimationsAppAPITests(EstimationsAppBaseTests):
-
+class FormCommentsAPITests(EstimationsAppAPITests):
     def setUp(self):
         super().setUp()
+
+    def _test_form_comments_detail(self, *args, **kwargs):
+        return self._test_form('estimation.estimations:form-comments-detail', *args, **kwargs)
+
+    def test_form_comments_detail_get_200(self):
+        self._test_form_comments_detail("get", None, status.HTTP_200_OK, reverse_args=self.app_form_1.id)
