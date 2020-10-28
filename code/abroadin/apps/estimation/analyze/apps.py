064e4d7e8b903b604dc99228bf2d338cf32d387e
hossein_7 <hossein.slayer@gmail.com>
2020-10-28 22:49:25 +0330

    chart items sorting added

--- code/abroadin/apps/estimation/analyze/apps.py
+++ code/abroadin/apps/estimation/analyze/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class AnalyzeConfig(AppConfig):
     name = 'abroadin.apps.estimation.analyze'
+
+    def ready(self):
+        import abroadin.apps.estimation.analyze.signals.handlers
