5dee31c1945752c3c80bb6306be716df3b575259
aryabartar <bartararya@gmail.com>
2020-11-01 14:11:32 +0330

    Update WantToApplyChanceSerializer

--- code/abroadin/apps/estimation/estimations/serializers.py
+++ code/abroadin/apps/estimation/estimations/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from abroadin.apps.estimation.form.models import WantToApply
 from abroadin.apps.estimation.estimations.classes import ValueRange
 from abroadin.apps.estimation.estimations.values import VALUES_WITH_ATTRS
+from apps.estimation.estimations.chances import AdmissionChance
 
 
 class WantToApplyChanceSerializer(serializers.Serializer):
@@ -16,37 +17,10 @@ class WantToApplyChanceSerializer(serializers.Serializer):
         ]
 
     def get_universities(self, obj):
-        universities_with_chance_list = []
-        value_range = ValueRange(VALUES_WITH_ATTRS["admission_chance_value_to_label"])
+        data = []
+        admission_chance = AdmissionChance(obj.student_detailed_info)
 
         for university in obj.universities.all().order_by('rank'):
-            rank = university.rank
+            data.append(admission_chance.get_university_chance_with_label(university))
 
-            universities_with_chance_list.append({
-                "university": university.name,
-                "rank": university.rank,
-                "chances": {
-                    "admission": value_range.find_value_attrs(value, label),
-                    "admission_value": None,
-                    "scholarship": None,
-                    "scholarship_value": None,
-                    "full_fund": None,
-                    "full_fund_value": None
-                }
-            }
-            )
-
-
-            elif 401 <= uni.rank:
-            append_dict["chances"] = {
-                "admission": "High",
-                "admission_value": 1,
-                "scholarship": "High",
-                "scholarship_value": 1,
-                "full_fund": "High",
-                "full_fund_value": 1
-            }
-
-        (append_dict)
-
-    return universities_with_chance_list
+        return data
