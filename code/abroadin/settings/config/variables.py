394bff80697152c728fec4ae97d8f1bb8f9f1494
hossein_7 <hossein.slayer@gmail.com>
2020-11-02 14:31:16 +0330

    adreesses changed to https

--- code/abroadin/settings/config/variables.py
+++ code/abroadin/settings/config/variables.py
@@ -1,6 +1,6 @@
 import os
 
 TIME_SLOT_SALE_DELETE_TIME = 24  # hours
-FRONTEND_URL = "http://abroadin.com/"
-BACKEND_URL = "http://api.abroadin.com/"
+FRONTEND_URL = "https://abroadin.com/"
+BACKEND_URL = "https://api.abroadin.com/"
 SERVER_NAME = SECRET_KEY = os.environ.get('SERVER_NAME')
