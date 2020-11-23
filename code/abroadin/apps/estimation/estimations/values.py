5683e922f0fa7876def90a885a0d4f333fe431f1
aryabartar <bartararya@gmail.com>
2020-11-23 19:58:33 +0330

    Update scores

--- code/abroadin/apps/estimation/estimations/values.py
+++ code/abroadin/apps/estimation/estimations/values.py
@@ -247,22 +247,29 @@ VALUES_WITH_ATTRS = {
     "1_20_university_rank_admission_chance": [
         {
             "start": None,
-            "end": 4/5,
+            "end": 4 / 5,
             "admission": 0.8,
             "scholarship": 0.6,
             "full_fund": 0.5
         },
         {
-            "start": 4/5,
-            "end": 3/5,
+            "start": 4 / 5,
+            "end": 3 / 5,
             "admission": 0.5,
             "scholarship": 0.2,
             "full_fund": 0.1
         },
         {
-            "start": 3/5,
-            "end": None,
+            "start": 3 / 5,
+            "end": 2 / 5,
             "admission": 0.3,
+            "scholarship": 0,
+            "full_fund": 0
+        },
+        {
+            "start": 2 / 5,
+            "end": None,
+            "admission": 0,
             "scholarship": 0,
             "full_fund": 0
         },
@@ -270,29 +277,29 @@ VALUES_WITH_ATTRS = {
     "21_100_university_rank_admission_chance": [
         {
             "start": None,
-            "end": 4/5,
+            "end": 4 / 5,
             "admission": 1,
             "scholarship": 0.95,
             "full_fund": 0.85
         },
         {
-            "start": 4/5,
-            "end": 3/5,
+            "start": 4 / 5,
+            "end": 3 / 5,
             "admission": 0.7,
             "scholarship": 0.6,
             "full_fund": 0.55
         },
         {
-            "start": 3/5,
-            "end": 2/5,
+            "start": 3 / 5,
+            "end": 2 / 5,
             "admission": 0.5,
             "scholarship": 0.2,
             "full_fund": 0.15
         },
         {
-            "start": 2/5,
+            "start": 2 / 5,
             "end": None,
-            "admission": 0.4,
+            "admission": 0.35,
             "scholarship": 0,
             "full_fund": 0
         },
@@ -300,29 +307,29 @@ VALUES_WITH_ATTRS = {
     "101_400_university_rank_admission_chance": [
         {
             "start": None,
-            "end": 4/5,
+            "end": 4 / 5,
             "admission": 1,
             "scholarship": 1,
             "full_fund": 1
         },
         {
-            "start": 4/5,
-            "end": 3/5,
+            "start": 4 / 5,
+            "end": 3 / 5,
             "admission": 1,
             "scholarship": 0.8,
             "full_fund": 0.7
         },
         {
-            "start": 3/5,
-            "end": 2/5,
+            "start": 3 / 5,
+            "end": 2 / 5,
             "admission": 0.6,
             "scholarship": 0.3,
             "full_fund": 0.2
         },
         {
-            "start": 2/5,
+            "start": 2 / 5,
             "end": None,
-            "admission": 0.3,
+            "admission": 0.4,
             "scholarship": 0,
             "full_fund": 0
         },
@@ -330,29 +337,29 @@ VALUES_WITH_ATTRS = {
     "401_above_university_rank_admission_chance": [
         {
             "start": None,
-            "end": 4/5,
+            "end": 4 / 5,
             "admission": 1,
             "scholarship": 1,
             "full_fund": 1
         },
         {
-            "start": 4/5,
-            "end": 3/5,
+            "start": 4 / 5,
+            "end": 3 / 5,
             "admission": 1,
             "scholarship": 1,
             "full_fund": 1
         },
         {
-            "start": 3/5,
-            "end": 2/5,
+            "start": 3 / 5,
+            "end": 2 / 5,
             "admission": 0.9,
             "scholarship": 0.5,
             "full_fund": 0.4
         },
         {
-            "start": 2/5,
+            "start": 2 / 5,
             "end": None,
-            "admission": 0.7,
+            "admission": 0.6,
             "scholarship": 0.2,
             "full_fund": 0.1
         },
