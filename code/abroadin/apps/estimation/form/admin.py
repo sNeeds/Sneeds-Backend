ff5c79c432b92b91b533e0b8db75788f3cb6b52a
aryabartar <bartararya@gmail.com>
2020-11-30 18:54:58 +0330

    Update is_complete

--- code/abroadin/apps/estimation/form/admin.py
+++ code/abroadin/apps/estimation/form/admin.py
@@ -95,7 +95,12 @@ class StudentDetailedInfoBaseAdmin(admin.ModelAdmin):
 @admin.register(models.StudentDetailedInfo)
 class StudentDetailedInfoAdmin(StudentDetailedInfoBaseAdmin):
     inlines = [WantToApplyInline] + StudentDetailedInfoBaseAdmin.inlines
-    list_display = ['id', 'user', 'value', 'rank', 'created', 'updated']
+    list_display = ['id', 'user', 'value', 'rank', 'created', 'updated', 'is_complete']
+
+    def is_complete(self, instance):
+        return instance.is_complete
+
+    is_complete.boolean = True
 
 
 @admin.register(models.Publication)
