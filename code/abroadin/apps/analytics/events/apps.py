1369a3826faeeba26bd1b0c7a16f8e2fc9743e35
aryabartar <bartararya@gmail.com>
2020-12-11 22:22:16 +0330

    Add posthog to project

--- code/abroadin/apps/analytics/events/apps.py
+++ code/abroadin/apps/analytics/events/apps.py
@@ -1,5 +1,15 @@
+import posthog
+
 from django.apps import AppConfig
+from abroadin.settings.secure.APIs import posthog as posthog_api
 
 
-class EventsConfig(AppConfig):
-    name = 'events'
+class EventConfig(AppConfig):
+    name = 'abroadin.apps.analytics.events'
+
+    def ready(self):
+        posthog.api_key = posthog_api
+        posthog.host = 'https://app.posthog.com'
+
+        posthog.capture('test-id', 'test-event')
+
