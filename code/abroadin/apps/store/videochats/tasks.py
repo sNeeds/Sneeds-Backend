ac12115b91b28b5f2e6dfc7f25e30d523700cb3a
aryabartar <bartararya@gmail.com>
2020-12-20 22:04:13 +0330

    Update delete_used_rooms

--- code/abroadin/apps/store/videochats/tasks.py
+++ code/abroadin/apps/store/videochats/tasks.py
@@ -26,6 +26,7 @@ def create_rooms_from_sold_time_slots():
     qs.update(used=True)
 
 
+@shared_task
 def delete_used_rooms():
     qs = Room.objects.filter(
         sold_time_slot__end_time__lte=timezone.now() - timezone.timedelta(minutes=5)
