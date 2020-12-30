4dc69acd49ff08d7a8802e346444224fdba6d655
aryabartar <bartararya@gmail.com>
2020-12-30 09:53:34 +0330

    Update orders endpoint

--- code/abroadin/apps/store/orders/serializers.py
+++ code/abroadin/apps/store/orders/serializers.py
@@ -13,72 +13,27 @@ from abroadin.apps.store.storePackages.serializers import SoldStorePaidPackagePh
 
 class OrderSerializer(serializers.ModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="order:order-detail", lookup_field='id', read_only=True)
-    sold_time_slot_sales = serializers.SerializerMethodField()
-    sold_class_products = serializers.SerializerMethodField()
-    sold_webinar_products = serializers.SerializerMethodField()
-    sold_store_paid_package_phases = serializers.SerializerMethodField()
-    used_discount = serializers.SerializerMethodField()
+    title = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ['id', 'url', 'order_id', 'status', 'subtotal', 'total',
-                  'sold_time_slot_sales', 'sold_class_products', 'sold_webinar_products',
-                  'sold_store_paid_package_phases', 'created', 'updated',
-                  'used_discount', 'time_slot_sales_number_discount',
-                  ]
+        fields = [
+            'id', 'url', 'title', 'order_id', 'status', 'subtotal', 'total', 'created', 'updated',
+        ]
         extra_kwargs = {
             'id': {'read_only': True},
+            'url': {'read_only': True},
+            'title': {'read_only': True},
             'order_id': {'read_only': True},
             'status': {'read_only': True},
-            'used_discount': {'read_only': True},
-            'time_slot_sales_number_discount': {'read_only': True},
-            'sold_class_products': {'read_only': True},
-            'sold_webinar_products': {'read_only': True},
-            'sold_store_paid_package_phases': {'read_only': True},
             'subtotal': {'read_only': True},
             'total': {'read_only': True},
             'created': {'read_only': True},
             'updated': {'read_only': True},
         }
 
-    def get_sold_time_slot_sales(self, obj):
-        sold_time_slot_sales = obj.sold_products.all().get_sold_time_slot_sales()
-        return SoldTimeSlotSaleSerializer(
-            sold_time_slot_sales, many=True, context={"request": self.context.get("request")}
-        ).data
-
-    def get_sold_basic_products(self, obj):
-        sold_basic_products = obj.sold_products.all().get_sold_basic_products()
-        return SoldBasicProductSerializer(
-            sold_basic_products, many=True, context={"request": self.context.get("request")}
-        ).data
-
-    def get_sold_class_products(self, obj):
-        sold_class_products = obj.sold_products.all().get_sold_class_products()
-        return SoldClassProductSerializer(
-            sold_class_products, many=True, context={"request": self.context.get("request")}
-        ).data
-
-    def get_sold_webinar_products(self, obj):
-        sold_webinar_products = obj.sold_products.all().get_sold_webinar_products()
-        return SoldWebinarProductSerializer(
-            sold_webinar_products, many=True, context={"request": self.context.get("request")}
-        ).data
-
-    def get_sold_store_paid_package_phases(self, obj):
-        sold_store_paid_package_phases = obj.sold_products.all().get_sold_store_paid_package_phases()
-        return SoldStorePaidPackagePhaseSerializer(
-            sold_store_paid_package_phases, many=True, context={"request": self.context.get("request")}
-        ).data
-
-    def get_used_discount(self, obj):
-        if obj.used_discount is None:
-            return None
-        else:
-            return {
-                "code": obj.used_discount.code,
-                "amount": obj.used_discount.amount
-            }
+    def get_title(self, obj):
+        return "Similar Admissions"
 
     def validate(self, attrs):
         user = self.context.get('request', None).user
@@ -92,6 +47,7 @@ class OrderSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = None
         request = self.context.get('request', None)
+
         if request and hasattr(request, "user"):
             user = request.user
 
