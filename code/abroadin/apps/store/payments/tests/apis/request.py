a670975dc631b6e3d86e5eef0797120f9f1652a3
aryabartar <bartararya@gmail.com>
2021-01-08 18:38:02 +0330

    Add create_payment

--- code/abroadin/apps/store/payments/tests/apis/request.py
+++ code/abroadin/apps/store/payments/tests/apis/request.py
@@ -8,18 +8,17 @@ User = get_user_model()
 
 
 class PaymentAPIRequestTests(PaymentAPIBaseTest):
-
     def setUp(self):
         super().setUp()
 
     def _test_request(self, *args, **kwargs):
         return self._endpoint_test_method('payment:request', *args, **kwargs)
 
-    def test_create_201(self):
-        def create_payment(user, cart):
-            data = self._test_request("post", user, status.HTTP_201_CREATED, data={"cartid": cart.id})
-            return data
+    def create_payment(self, user, cart):
+        data = self._test_request("post", user, status.HTTP_201_CREATED, data={"cartid": cart.id})
+        return data
 
+    def test_create_201(self):
         def check_non_zero_price_cart_response(data):
             self.assertNotEqual(data.get("redirect"), None)
 
@@ -28,7 +27,16 @@ class PaymentAPIRequestTests(PaymentAPIBaseTest):
             self.assertEqual(data.get("ReflD"), "00000000")
             self.assertNotEqual(data.get("order"), None)
 
-        data = create_payment(self.user1, self.a_cart1)
+        data = self.create_payment(self.user1, self.a_cart1)
+        check_non_zero_price_cart_response(data)
+
+        data = self.create_payment(self.user1, self.a_cart2)
+        check_zero_price_cart_response(data)
+
+    def test_create_400(self):
+
+
+        data = self.create_payment(self.user1, self.a_cart3)
         check_non_zero_price_cart_response(data)
 
         data = create_payment(self.user1, self.a_cart2)
