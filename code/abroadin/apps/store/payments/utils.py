67b83852ae5635a46757b4865331e5f7883e55b3
aryabartar <bartararya@gmail.com>
2021-01-10 11:34:01 +0330

    Remove DepositInfo model and codes

--- code/abroadin/apps/store/payments/utils.py
+++ code/abroadin/apps/store/payments/utils.py
@@ -6,14 +6,3 @@ def random_string_generator(size=12, chars=string.ascii_lowercase + string.digit
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def unique_consultant_deposit_info_id_generator(instance):
-    """
-    This is for a Django project with an consultant_deposit_info_id field
-    """
-    new_consultant_deposit_info_id = random_string_generator()
-
-    Klass = instance.__class__
-    qs_exists = Klass.objects.filter(consultant_deposit_info_id=new_consultant_deposit_info_id).exists()
-    if qs_exists:
-        return unique_consultant_deposit_info_id_generator(instance)
-    return new_consultant_deposit_info_id
