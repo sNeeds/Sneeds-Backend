7c14eb2757059a66c50fbc66d4ffe3b09a1a571d
aryabartar <bartararya@gmail.com>
2021-01-30 08:38:03 +0330

    Update permissions

--- code/abroadin/apps/data/applydata/permissions.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.contenttypes.models import ContentType
-
-from rest_framework import permissions
-
-from abroadin.apps.estimation.form.models import StudentDetailedInfo, StudentDetailedInfoBase
