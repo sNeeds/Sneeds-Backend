152a8ffb52740e4314ce64383c9762307911f236
hossein_7 <hossein.slayer@gmail.com>
2021-01-31 12:43:39 +0330

    pagination added to apply profile

--- code/abroadin/apps/applyprofile/views.py
+++ code/abroadin/apps/applyprofile/views.py
@@ -1,12 +1,20 @@
+from rest_framework.pagination import PageNumberPagination
+
 from abroadin.base.api.generics import CListAPIView, CRetrieveAPIView
 
 from .serializers import ApplyProfileSerializer, FullAdmissionSerializer
 from .models import ApplyProfile, Admission
 
 
+class SmallResultsSetPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 20
+
 class ApplyProfileAPIView(CListAPIView):
     queryset = ApplyProfile.objects.all()
     serializer_class = ApplyProfileSerializer
+    pagination_class = SmallResultsSetPagination
 
 
 class ApplyProfileDetailAPIView(CRetrieveAPIView):
