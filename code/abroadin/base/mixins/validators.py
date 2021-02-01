a05556b733aee318cf6b69e5af83e8821c712b43
aryabartar <bartararya@gmail.com>
2021-02-01 17:58:01 +0330

    Fix same object multiple content_type bug

--- code/abroadin/base/mixins/validators.py
+++ code/abroadin/base/mixins/validators.py
@@ -10,7 +10,7 @@ class GenericForeignkeyUniqueTogetherValidationMixin:
             content_type_obj = getattr(self, field_name)
         except AttributeError:
             raise AttributeError(
-                f"The {field_name} field name is not a valid content type field "
+                f"The {field_name} field name is not a valid  field "
                 f"name for {self.__class__.__name__} model."
             )
         if not isinstance(content_type_obj, ContentType):
@@ -18,15 +18,21 @@ class GenericForeignkeyUniqueTogetherValidationMixin:
                 f"The {field_name} field name is not a ContentType instance "
                 f"for {self.__class__.__name__} model."
             )
-
         return content_type_obj
 
     def _generic_fk_unique_together_validator(self, fields):
         lookup_kwargs = {}
         for field in fields:
             lookup_kwargs[field] = getattr(self, field)
-        exists = self.__class__.objects.filter(**lookup_kwargs).exists()
-        if exists:
+
+        model_class = self.__class__
+        qs = model_class._default_manager.filter(**lookup_kwargs)
+
+        model_class_pk = self._get_pk_val(model_class._meta)
+        if not self._state.adding and model_class_pk is not None:
+            qs = qs.exclude(pk=model_class_pk)
+
+        if qs.exists():
             raise ValidationError(
                 {NON_FIELD_ERRORS: _(f"The fields {fields} must make a unique set.")}
             )
@@ -36,8 +42,7 @@ class GenericForeignkeyUniqueTogetherValidationMixin:
             ct_obj = self._get_content_type_obj(ct_field_name)
             fields = apps.get(ct_obj.app_label + '__' + ct_obj.model, None)
             if fields:
-                fields.insert(0, ct_field_name)
-                self._generic_fk_unique_together_validator(fields)
+                self._generic_fk_unique_together_validator([ct_field_name] + fields)
 
         return super().validate_unique(exclude)
 
