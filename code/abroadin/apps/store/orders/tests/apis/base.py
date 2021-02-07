356bd90ebc4a352410e857960717290a768f84a0
hossein_7 <hossein.slayer@gmail.com>
2021-02-07 18:20:23 +0330

    bug fixed

--- code/abroadin/apps/store/orders/tests/apis/base.py
+++ code/abroadin/apps/store/orders/tests/apis/base.py
@@ -26,3 +26,5 @@ class OrderAPITestBase(OrderTestBase):
 
     def test_list_orders_patch_405(self):
         res = self._test_order_list('patch', self.user1, status.HTTP_405_METHOD_NOT_ALLOWED,  data={})
+
+
