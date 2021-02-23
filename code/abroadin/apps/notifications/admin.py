068e35498d72942a6d1036eed25f504e152295c7
hossein_7 <hossein.slayer@gmail.com>
2021-02-23 12:37:45 +0330

    notfication app is cleaned now

--- code/abroadin/apps/notifications/admin.py
+++ code/abroadin/apps/notifications/admin.py
@@ -2,23 +2,10 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import Notification, EmailNotification, SoldTimeSlotReminderEmailNotification, \
-    SoldTimeSlotChangedEmailNotification
+from .models import EmailNotification
 
 
 @admin.register(EmailNotification)
-class DiscountAdmin(admin.ModelAdmin):
-    list_display = ['email', 'sent', 'send_date']
-    readonly_fields = ['created', 'updated', 'sent', 'data_json', 'get_data_dict']
-
-
-@admin.register(SoldTimeSlotReminderEmailNotification)
-class DiscountAdmin(admin.ModelAdmin):
-    list_display = ['email', 'sent', 'send_date']
-    readonly_fields = ['created', 'updated', 'sent', 'data_json', 'get_data_dict']
-
-
-@admin.register(SoldTimeSlotChangedEmailNotification)
-class DiscountAdmin(admin.ModelAdmin):
+class EmailNotificationAdmin(admin.ModelAdmin):
     list_display = ['email', 'sent', 'send_date']
     readonly_fields = ['created', 'updated', 'sent', 'data_json', 'get_data_dict']
