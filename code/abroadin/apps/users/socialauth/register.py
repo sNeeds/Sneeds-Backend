b8fc5947e240a3841e79d894a326df5962049121
aryabartar <bartararya@gmail.com>
2021-02-27 14:11:15 +0330

    Update login_register_social_user'

--- code/abroadin/apps/users/socialauth/register.py
+++ code/abroadin/apps/users/socialauth/register.py
@@ -1,13 +1,12 @@
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import RefreshToken
 
-from abroadin.apps.users.customAuth.serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
 
 
-def _register_user(data):
-    user = User.objects.create_user(**data)
+def _register_user(*args, **kwargs):
+    user = User.objects.create_user(**kwargs)
     user.is_verified = True
     user.save()
 
@@ -30,23 +29,17 @@ def login_register_social_user(email, provider, first_name, last_name):
     filtered_user = User.objects.filter(email=email)
     user_exists = filtered_user.exists()
 
-    data = {
-        'email': email,
-        'first_name': first_name,
-        'last_name': last_name,
-        'auth_provider': provider
-    }
-
     if user_exists:
         user = filtered_user[0]
     else:
-        user = _register_user(data)
+        user = _register_user(
+            email=email, first_name=first_name, last_name=last_name,
+            auth_provider=provider
+        )
 
-    user_serializer = UserSerializer(user)
     tokens = get_user_jwt_tokens(user)
 
     return {
-        'user': user_serializer.data,
         'access': tokens['access'],
         'refresh': tokens['refresh']
     }
