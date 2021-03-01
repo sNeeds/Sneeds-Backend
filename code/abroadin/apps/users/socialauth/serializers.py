ca2ba8f49410fc8d101a907ea4a9eb788763177a
aryabartar <bartararya@gmail.com>
2021-03-01 18:44:51 +0330

    Add FacebookSocialAuthSerializer

--- code/abroadin/apps/users/socialauth/serializers.py
+++ code/abroadin/apps/users/socialauth/serializers.py
@@ -1,12 +1,13 @@
+from google.auth.exceptions import GoogleAuthError
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, login_rule, user_eligible_for_login
+
 from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.models import update_last_login
-from google.auth.exceptions import GoogleAuthError
 
 from rest_framework import serializers
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, login_rule, user_eligible_for_login
-from rest_framework_simplejwt.settings import api_settings
 
+from . import facebook
 from .register import login_register_social_user
 from .google import Google
 
@@ -79,3 +80,29 @@ class GoogleSocialAuthSerializer(serializers.Serializer):
         data = super().validate(attrs)
         data['user'] = self.user
         return data
+
+
+class FacebookSocialAuthSerializer(serializers.Serializer):
+    auth_token = serializers.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = None
+
+    def validate_auth_token(self, auth_token):
+        user_data = facebook.Facebook.validate(auth_token)
+
+        try:
+            email = user_data['email']
+            first_name = user_data['first_name']
+            last_name = user_data['last_name']
+        except KeyError as e:
+            raise ValidationError(
+                "Error getting user data from facebook, Key error was:" + e.__str__()
+            )
+
+        provider = User.AuthProviderTypeChoices.FACEBOOK
+        self.user = login_register_social_user(
+            provider=provider, email=email, first_name=first_name, last_name=last_name
+        )
+        return auth_token
