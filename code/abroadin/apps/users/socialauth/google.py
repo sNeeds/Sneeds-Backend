93cc430c3d691327165663daa1248a50b488056c
aryabartar <bartararya@gmail.com>
2021-03-03 09:42:58 +0330

    Removed is_married from required fields

--- code/abroadin/apps/users/socialauth/google.py
+++ code/abroadin/apps/users/socialauth/google.py
@@ -1,4 +1,7 @@
-import requests
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
 from google.auth.exceptions import GoogleAuthError
 from google.auth.transport import requests as transport_requests
 from google.oauth2 import id_token
@@ -10,32 +13,55 @@ class Google:
     """Google class to fetch the user info and return it"""
 
     @staticmethod
+    def credentials_to_dict(credentials):
+        return {
+            'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'id_token': credentials.id_token,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes
+        }
+
+    @staticmethod
     def validate(auth_token):
         """
         validate method Queries the Google oAUTH2 api to fetch the user info
         """
-        data = {
-            'code': auth_token,
-            'redirect_uri': 'https://abroadin.com/auth/login',
-            'client_id': GOOGLE_CLIENT_ID,
-            'client_secret': GOOGLE_CLIENT_SECRET,
-            'grant_type': 'authorization_code',
+        client_conf = {
+            "web":
+                {
+                    "client_id": GOOGLE_CLIENT_ID,
+                    "client_secret": GOOGLE_CLIENT_SECRET,
+                    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                    "token_uri": "https://oauth2.googleapis.com/token",
+                }
         }
-        exchange = requests.post(
-            'https://oauth2.googleapis.com/token',
-            data=data
-        ).json()
 
-        id_token_code = exchange['id_token']
+        flow = google_auth_oauthlib.flow.Flow.from_client_config(
+            client_conf,
+            scopes=[
+                'https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/userinfo.profile',
+                'openid'
+            ],
+        )
+        flow.redirect_uri = 'https://abroadin.com/auth/login'
+
+        flow.fetch_token(code=auth_token)
+        credentials = Google.credentials_to_dict(flow.credentials)
+
+        id_token_code = credentials['id_token']
 
         try:
-            idinfo = id_token.verify_oauth2_token(
+            id_info = id_token.verify_oauth2_token(
                 id_token_code, transport_requests.Request()
             )
         except Exception as e:
             raise GoogleAuthError(e.__str__())
 
-        if 'accounts.google.com' not in idinfo['iss']:
-            raise GoogleAuthError('Wrong iss in idinfo')
+        if 'accounts.google.com' not in id_info['iss']:
+            raise GoogleAuthError('Wrong iss in id_info')
 
-        return idinfo
+        return id_info
