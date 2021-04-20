b2462038bff8b0532dda16bdd0cca8317697be01
aryabartar <bartararya@gmail.com>
2021-04-20 18:22:08 +0430

    Update sdi endpoint

--- code/abroadin/apps/estimation/form/urls.py
+++ code/abroadin/apps/estimation/form/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = "estimation.form"
 
 urlpatterns = [
-    path('student-detailed-infos/', views.StudentDetailedInfoListCreateView.as_view(),
+    path('student-detailed-info/', views.StudentDetailedInfoListCreateView.as_view(),
          name='student-detailed-info-list'),
 
-    path('student-detailed-infos/<int:id>/', views.StudentDetailedInfoRetrieveUpdateView.as_view(),
+    path('student-detailed-info/<int:id>/', views.StudentDetailedInfoRetrieveUpdateView.as_view(),
          name='student-detailed-info-detail'),
 
     path('user-student-detailed-info/<int:user_id>/', views.UserStudentDetailedInfoRetrieveAPIView.as_view(),
